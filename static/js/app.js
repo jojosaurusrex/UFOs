8b8900699b4468a804f4849c0d303661ac6640e4
Joseph Michael Becerra <becerra.joseph424@gmail.com>
2022-05-08 17:35:38 -0700

    update: not printing table

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,9 @@
-// // import the data from data.js
-// const tableData = data;
+// import the data from data.js
+const tableData = data;
 
-// // Reference the HTML table using d3
-// var tbody = d3.select("tbody");
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
 
-// Simple JavaScript console.log statement
-function printHello() {
-    console.log("Hello there!");
-  }
-
-  // Takes two numbers and adds them
-// function addition(a, b) {
-//   return a + b;
-// }
-printHello();
-// ^Converted to an arrow function
-addition = (a, b) => a + b;
-
-// Functions can call other functions
-// function doubleAddition(c, d) {
-//   var total = addition(c, d) * 2;
-//   return total;
-// }
-
-// ^Converted to an arrow function
-doubleAddition = (c, d) => addition(c, d)*2;
 
 function buildTable(data) {
   tbody.html(""); // clears data
@@ -33,11 +12,13 @@ function buildTable(data) {
   data.forEach((dataRow) => { 
     let row = tbody.append("tr");
   
-    Object.values(dataRow).forEach((val) => { // dataRow -> values go into dataRow
+    Object.values(dataRow).forEach((val) => { 
+      // dataRow -> values go into dataRow
       // the forEach((val) specifies we want one object per row
       let cell = row.append("td");
       // ^set up the action of appending data into table data tag <td>
-      cell.text(val); // actual addition of values into the data tag
+      cell.text(val); 
+      // actual addition of values into the data tag
     });
   
    }); 
@@ -61,7 +42,7 @@ function handleClick() {
     // w/in JavaScript????
   
   if (date) {
-    filteredData - filteredData.filter(row => row.datetime === date);
+    filteredData = filteredData.filter(row => row.datetime === date);
   };
 
 
